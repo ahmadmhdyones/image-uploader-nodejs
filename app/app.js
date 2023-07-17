47e0af74ff492b56963d0a1af7a27c5cb6b91964
Ahmad Yones <ahmadmhdyones@gmail.com>
2023-07-17 23:30:10 +0300

    Update app middleware chain
    + New middleware:
      - mongoSanitize
      - cookieParser
      - compression
      - helmet
      - xss
      - hpp
    
    + Others:
      - Enable trust proxy.
      - Reorder the middlewares calls.
      - Install new packages.

--- app/app.js
+++ app/app.js
@@ -1,8 +1,14 @@
 const path = require('path');
 
 const express = require('express');
+const mongoSanitize = require('express-mongo-sanitize');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const cors = require('cors');
+const hpp = require('hpp');
 require('colors');
 
 const config = require('./config/app.conf');
@@ -11,36 +17,45 @@ const {
   checkPayload,
   addRequestStartTime,
   errorHandler,
-  parseFormData,
 } = require('./middleware');
 
 const app = express();
 
-// Logging middleware
-app.use(morgan('dev', config.morganOptions[0]));
-app.use(morgan('common', config.morganOptions[1]));
+app.enable('trust proxy'); // Enable trust for proxy servers
 
-// CORS middleware
-app.use(cors(config.corsOptions));
+app.use(morgan('dev', config.morganOptions[0])); // For console logging
+app.use(morgan('common', config.morganOptions[1])); // For file logging
 
-// Body parsing middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(parseFormData);
+app.use(cors(config.corsOptions)); // Enable cross-origin resource sharing
 
-// Serving static and storage files middleware
+// app.use(checkPayload); // Check for the payload size of incoming requests
+app.use(addRequestStartTime); // Add the start time of incoming requests
+
 app.use(
   config.staticFilesPrefix,
   express.static(path.join(config.basedir, 'public'))
-);
+); // Serve static files from the public directory
 app.use(
   config.storageFilesPrefix,
   express.static(path.join(config.basedir, 'uploads'))
-);
+); // Serve storage files from the uploads directory
 
-app.use(addRequestStartTime);
-app.use(checkPayload);
-app.use(router);
-app.use(errorHandler);
+app.use(helmet()); // Add various HTTP headers to enhance security
+
+app.use(express.json(config.parser[0])); // Parse JSON-encoded bodies
+app.use(express.urlencoded(config.parser[1])); // Parse URL-encoded bodies
+app.use(cookieParser()); // Parse cookie headers
+
+app.use(mongoSanitize()); // Sanitize user input to prevent MongoDB Injection attacks
+app.use(xss()); // Sanitize user input to prevent Cross-Site Scripting (XSS) attacks
+
+app.use(hpp()); // Prevent HTTP Parameter Pollution attacks
+
+// TODO: test the impact of compression
+app.use(compression()); // Compress response bodies
+
+app.use(router); // Mount the router middleware to handle incoming requests
+
+app.use(errorHandler); // Handle errors that occur during request processing
 
 module.exports = app;
